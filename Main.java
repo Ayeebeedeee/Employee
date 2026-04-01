1009be93147f6b17be9252244dac3b9a1fb20b3f
Ayeebeedeee <125183053+Ayeebeedeee@users.noreply.github.com>
2026-04-01 10:20:32 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,7 +1,69 @@
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-        EmployeeDAO obj = new EmployeeDAO();
-        obj.insertEmployee("Abhyudaya", 50000);
+
+        Scanner sc = new Scanner(System.in);
+        EmployeeDAO dao = new EmployeeDAO();
+
+        while (true) {
+
+            System.out.println("\n--- Employee Management System ---");
+            System.out.println("1. Add Employee");
+            System.out.println("2. View Employees");
+            System.out.println("3. Update Employee");
+            System.out.println("4. Delete Employee");
+            System.out.println("5. Exit");
+
+            System.out.print("Enter choice: ");
+            int choice = sc.nextInt();
+            sc.nextLine(); // clear buffer
+
+            switch (choice) {
+
+                case 1:
+                    System.out.print("Enter Name: ");
+                    String name = sc.nextLine();
+
+                    System.out.print("Enter Salary: ");
+                    int salary = sc.nextInt();
+
+                    dao.insertEmployee(name, salary);
+                    break;
+
+                case 2:
+                    dao.getEmployee();
+                    break;
+
+                case 3:
+                    System.out.print("Enter ID: ");
+                    int id = sc.nextInt();
+                    sc.nextLine();
+
+                    System.out.print("Enter New Name: ");
+                    String newName = sc.nextLine();
+
+                    System.out.print("Enter New Salary: ");
+                    int newSalary = sc.nextInt();
+
+                    dao.updateEmployee(id, newName, newSalary);
+                    break;
+
+                case 4:
+                    System.out.print("Enter ID: ");
+                    int delId = sc.nextInt();
+
+                    dao.delEmployee(delId);
+                    break;
+
+                case 5:
+                    System.out.println("Exiting...");
+                    return;
+
+                default:
+                    System.out.println("Invalid choice");
+            }
+        }
     }
 }
